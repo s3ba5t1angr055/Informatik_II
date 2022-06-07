ec4328d04db137c19321426e6cbd2e0d5824d9f3
s3ba5t1angr055 <107070677+s3ba5t1angr055@users.noreply.github.com>
2022-06-07 20:08:22 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,27 @@
+//
+//  main.cpp
+//  3.1.Teilaufgabe5
+//
+//  Created by Sebastian Groß on 07.06.22.
+//
+
+#include <iostream>
+using namespace std;
+
+int n = 5;
+
+int main(int argc, const char * argv[]) {
+    for (int i = -n; i <= n; i++) {
+        for (int j = -n; j <= n; j++) {
+            if (i*i + j*j < n*n) {
+                    cout << "X";
+                } else {
+                    cout << " ";
+                }
+            }
+        cout << endl;
+        }
+    }
+
+
+
